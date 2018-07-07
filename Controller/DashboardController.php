3cf9bb72d9fa4a028baa5fa40584092be8483623
Guite <info@guite.de>
2018-07-07 10:05:56 +0200

    updated usage of obsolete annotation class

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -11,10 +11,10 @@
 
 namespace Phaidon\PiwikModule\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Zikula\Core\Controller\AbstractController;
 use Zikula\ThemeModule\Engine\Annotation\Theme;
